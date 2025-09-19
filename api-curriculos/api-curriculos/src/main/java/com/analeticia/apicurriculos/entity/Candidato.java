ddf62a5784894cd67403bd29772b61425f255085
Ana Letícia Cabral do Rosário <cabraldorosarioanaleticia@gmail.com>
2025-09-19 20:50:18 -0300

    Update Candidato.java

--- api-curriculos/api-curriculos/src/main/java/com/analeticia/apicurriculos/entity/Candidato.java
+++ api-curriculos/api-curriculos/src/main/java/com/analeticia/apicurriculos/entity/Candidato.java
@@ -1,5 +1,17 @@
 package com.analeticia.apicurriculos.entity;
 
+import java.time.LocalDate;
+
 public class Candidato {
 
+	private long id;
+	private String nome;
+	private String sobrenome;
+	private String genero;
+	private LocalDate nascimento;
+	private String cpf;
+	private String email;
+	private String telefone;
+	private String celular;
+	private String sobre;
 }
