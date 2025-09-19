45d512b212d634775f00f1b2b239f3b8a46dd025
Ana Letícia Cabral do Rosário <cabraldorosarioanaleticia@gmail.com>
2025-09-19 20:28:22 -0300

    Estrutura do projeto

---
+++ api-curriculos/api-curriculos/src/main/java/com/analeticia/apicurriculos/ProjetoConnectionCvApplication.java
@@ -0,0 +1,13 @@
+package com.analeticia.apicurriculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoConnectionCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoConnectionCvApplication.class, args);
+	}
+
+}
---
+++ api-curriculos/api-curriculos/src/test/java/com/analeticia/apicurriculos/ProjetoConnectionCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analeticia.apicurriculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoConnectionCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
