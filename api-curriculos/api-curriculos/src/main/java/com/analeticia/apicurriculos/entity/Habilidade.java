1902cc44f92d2e744a8c950b8dc22562c096c42b
Ana Letícia Cabral do Rosário <cabraldorosarioanaleticia@gmail.com>
2025-09-29 21:22:25 -0300

    Create Habilidade.java

---
+++ api-curriculos/api-curriculos/src/main/java/com/analeticia/apicurriculos/entity/Habilidade.java
@@ -0,0 +1,40 @@
+package com.analeticia.apicurriculos.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Habilidade")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Habilidade {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_habilidade")
+	private Long idHabilidade;
+	
+	@NotNull(message = "Campo NOME é obrigatório")
+	private String nome;
+	
+	private String descricao;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "candidato_id")
+	private Candidato candidato;
+	
+}
